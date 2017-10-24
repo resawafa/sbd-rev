9418b21faf28d8f4343bb631df3c67bc5a0efe96
resas <resas@RESAWAFA>
2017-10-24 18:23:15 +0700

    tampil data mobil

---
+++ src/main/java/com/sbd12/sewamobil/Pkg_Data_Mobil/DataMobilTableModel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbd12.sewamobil.Pkg_Data_Mobil;
+
+import com.sbd12.sewamobil.Pkg_Merk_Mobil.MerkMobil;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author resas
+ */
+public class DataMobilTableModel extends AbstractTableModel{
+    private List<DataMobil> data;
+    private String[] nameField={"No Polisi","Merk","Owner"};
+    
+    public void setData(List<DataMobil> data)
+    {
+        this.data=data;
+    }
+    
+    @Override
+    public int getRowCount() {
+      return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+      return nameField.length; }
+
+    @Override
+    public Object getValueAt(int baris, int kolom) {
+        
+        DataMobil kst=data.get(baris);
+        switch(kolom)
+        {
+            case 0: return kst.getNo_pol();
+            case 1: return kst.getNama_mobil();
+            case 2: return kst.getNama_ow();
+            default : return null;
+        }
+    }
+    @Override
+    public String getColumnName(int column)
+    {
+        return nameField[column];
+    }
+}
